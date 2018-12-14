e57fe1d614ef3ab09144498efd7cd2895fae1d79
Bret Hudson <bret@brethudson.com>
2018-12-13 21:34:15 -0600

    Completed day 15

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const irandom = (n) => Math.floor(Math.random() * n);
 const choose = (...args) => args[irandom(args.length)];
 
-let daysCompleted = 14;
+let daysCompleted = 15;
 let worker = null;
 let openWorker = (id, input) => {
 	closeWorker();
