e00557a3a63ea312263ac812c5609e022d1b4f7f
Bret Hudson <bret@brethudson.com>
2023-12-06 01:56:33 -0800

    2023 Day 1 / Part 2

--- day-1.js
+++ day-1.js
@@ -1,11 +1,76 @@
-export const solution = (input) => {
-	let answers = [null, null];
+// note: indices are off by one
+const spelledDigits = [
+	'one',
+	'two',
+	'three',
+	'four',
+	'five',
+	'six',
+	'seven',
+	'eight',
+	'nine',
+];
 
-	answers[0] = input
-		.split('\n')
-		.map((line) => line.match(/\d/g))
-		.map((digits) => +(digits[0] + digits.at(-1)))
+const longestWordLength = spelledDigits.reduce(
+	(acc, val) => Math.max(acc, val.length),
+	0,
+);
+
+// original solution
+// input
+// 		.split('\n')
+// 		.map((line) => line.match(/\d/g))
+// 		.map((digits) => +(digits[0] + digits.at(-1)))
+// 		.reduce((acc, val) => acc + val, 0);
+
+const computeCalibrationSum = (input, useWords) => {
+	let cursor = 0;
+	let line = 0;
+	let lineDigits = [[]];
+	let consumed = 1;
+	for (; cursor < input.length; cursor += consumed) {
+		const nextCharacter = input.substring(cursor, cursor + 1);
+
+		switch (nextCharacter) {
+			case '\n': {
+				++line;
+				lineDigits.push([]);
+				continue;
+			}
+
+			// super cursed :) enjoy
+			case nextCharacter.match(/\d/)?.[0]: {
+				lineDigits[line].push(+nextCharacter);
+				continue;
+			}
+
+			default: {
+				if (!useWords) continue;
+
+				const possibleDigit = input.substring(
+					cursor,
+					cursor + longestWordLength,
+				);
+
+				const digit = spelledDigits.find((d) =>
+					possibleDigit.startsWith(d),
+				);
+				if (digit) {
+					const number =
+						spelledDigits.findIndex((d) => d === digit) + 1;
+					lineDigits[line].push(number);
+				}
+			}
+		}
+	}
+
+	return lineDigits
+		.map((digits) => digits[0] * 10 + digits.at(-1))
 		.reduce((acc, val) => acc + val, 0);
+};
 
-	return answers;
+export const solution = (input) => {
+	return [false, true].map((useWords) =>
+		computeCalibrationSum(input, useWords),
+	);
 };
