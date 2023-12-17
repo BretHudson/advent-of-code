28da4f770d9a20155504dd49bac5dca88482bcdf
Bret Hudson <bret@brethudson.com>
2023-12-17 11:18:43 -0800

    2023 Day 5 / Part 1

---
+++ 2023/solutions/day-5.js
@@ -0,0 +1,53 @@
+export const solution = (input) => {
+	const answers = [null, null];
+
+	const [seedsStr, ...sections] = input.split('\n\n');
+
+	const seeds = [...seedsStr.matchAll(/\d+/g)].map((v) => +v);
+
+	const maps = sections.reduce((acc, str) => {
+		const [nameStr, ...values] = str.split('\n');
+
+		const match = nameStr.match(/(?<srcCat>\w+)-to-(?<destCat>\w+) map:/);
+		const { srcCat, destCat } = match.groups;
+
+		const ranges = values.map((value) => {
+			const [destStart, srcStart, range] = value
+				.split(' ')
+				.map((v) => +v);
+			return { destStart, srcStart, range };
+		});
+
+		acc[srcCat] = { destCat, ranges };
+
+		return acc;
+	}, {});
+
+	const convertSeedToLocation = (seed) => {
+		let srcKey = 'seed';
+		let curValue = seed;
+		while (maps[srcKey]) {
+			let curMap = maps[srcKey];
+
+			const range = curMap.ranges.find(({ srcStart, range }) => {
+				return curValue >= srcStart && curValue < srcStart + range;
+			});
+
+			// note: if no range is found, we just re-use the same value
+			if (range) {
+				const { srcStart, destStart } = range;
+				const diff = curValue - srcStart;
+				curValue = destStart + diff;
+			}
+
+			srcKey = curMap.destCat;
+		}
+		return curValue;
+	};
+
+	const locations = seeds.map(convertSeedToLocation);
+
+	answers[0] = [...locations].sort((a, b) => a - b)[0];
+
+	return answers;
+};
