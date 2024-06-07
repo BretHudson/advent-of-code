42e9cf172c038da99ab101aabf4ef8955ada7b6a
Bret Hudson <bret@brethudson.com>
2024-06-07 13:45:27 -0700

    2023 Day 6 / Part 2

--- 2023/solutions/day-6.js
+++ 2023/solutions/day-6.js
@@ -5,13 +5,25 @@ export const solution = (input) => {
 		return [...line.matchAll(/(\d+)/g)].map((v) => v[1]);
 	});
 
+	const calculateDistance = (time, hold) => {
+		return time * hold - hold * hold;
+	};
+
 	const calculateWaysToWin = (time, dist) => {
-		let wins = 0;
-		for (let hold = 0; hold <= time + 1; ++hold) {
-			const d = time * hold - hold * hold;
-			if (d > dist) ++wins;
-		}
-		return wins;
+		const b = time;
+		const c = -dist;
+
+		const x1 = Math.ceil((-b + Math.sqrt(b * b + 4 * c)) / -2 + 0.00001);
+		const x2 = Math.floor((-b - Math.sqrt(b * b + 4 * c)) / -2 - 0.00001);
+		return x2 - x1 + 1;
+
+		/// loop method, too slow for part 2
+		// let wins = 0;
+		// for (let hold = 0; hold <= time + 1; ++hold) {
+		// 	const d = time * hold - hold * hold;
+		// 	if (d > dist) ++wins;
+		// }
+		// return wins;
 	};
 
 	const waysToWin = [];
@@ -22,7 +34,11 @@ export const solution = (input) => {
 		waysToWin.push(calculateWaysToWin(time, dist));
 	}
 
+	const totalTime = times.join('');
+	const totalDist = distances.join('');
+
 	answers[0] = waysToWin.reduce((a, v) => a * v, 1);
+	answers[1] = calculateWaysToWin(totalTime, totalDist);
 
 	return answers;
 };
