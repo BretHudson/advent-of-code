9662ff7e06d642ff896d5153a95b65f39ed911d0
Bret Hudson <bret@brethudson.com>
2024-06-10 17:01:44 -0700

    2023 Day 20 / Part 2

--- 2023/solutions/day-20.js
+++ 2023/solutions/day-20.js
@@ -8,22 +8,16 @@ export const solution = (input) => {
 	};
 
 	const modules = input.split('\n').map((line) => {
-		const [prefix, list] = line.split(' -> ');
+		const [prefix, listStr] = line.split(' -> ');
+		const list = listStr.split(', ');
 
 		const type = prefix[0];
 		const name = type === 'b' ? prefix : prefix.slice(1);
 
-		return {
-			name,
-			prefix,
-			list: list.split(', '),
-			type,
-		};
+		return { name, prefix, list, type };
 	});
 
-	const moduleGroups = Object.groupBy(modules, ({ type }) => {
-		return type;
-	});
+	const moduleGroups = Object.groupBy(modules, ({ type }) => type);
 
 	const moduleMap = new Map(modules.map((module) => [module.name, module]));
 	const flipFlopState = new Map(
@@ -32,7 +26,7 @@ export const solution = (input) => {
 	const conjunctionMemory = new Map(
 		moduleGroups[TYPE.CONJUNCTION].map((module) => {
 			const memory = new Map(
-				moduleGroups[TYPE.FLIP_FLOP]
+				modules
 					.filter((flipFlopModule) => {
 						return flipFlopModule.list.includes(module.name);
 					})
@@ -42,18 +36,34 @@ export const solution = (input) => {
 		}),
 	);
 
+	const target = 'rx';
+	const findFeeders = (target) => {
+		return modules
+			.filter((module) => module.list.includes(target))
+			.map(({ name }) => name);
+	};
+
+	const rxFeed = findFeeders(target)[0];
+	const rxFeedFeeds = findFeeders(rxFeed);
+	const feedHits = Array.from({ length: rxFeedFeeds.length });
+
 	const queue = [];
+	let buttonPresses = 0;
 	let pulseCount = [0, 0];
 	const addToQueue = (name, from, pulse) => {
+		if (!pulse && rxFeedFeeds.includes(name)) {
+			feedHits[rxFeedFeeds.indexOf(name)] = buttonPresses;
+		}
+
 		++pulseCount[+pulse];
 		if (!moduleMap.has(name)) return;
 		queue.push({ module: moduleMap.get(name), from, pulse });
 	};
 
 	const pushButton = () => {
+		++buttonPresses;
 		addToQueue('broadcaster', 'button', false);
 
-		let i = 0;
 		while (queue.length) {
 			const { module, from, pulse } = queue.shift();
 
@@ -84,23 +94,21 @@ export const solution = (input) => {
 		}
 	};
 
-	const allFalse = (map) => map.values().every((v) => v === false);
-	let loop = 1000;
 	for (let i = 0; i < 1000; ++i) {
 		pushButton();
-
-		const flipFlopReset = allFalse(flipFlopState);
-		const memoryReset = conjunctionMemory.values().every(allFalse);
-
-		if (flipFlopReset && memoryReset) {
-			loop = i + 1;
-			break;
-		}
 	}
 
-	const scale = 1000 / loop;
+	answers[0] = pulseCount[0] * pulseCount[1];
 
-	answers[0] = pulseCount[0] * pulseCount[1] * scale * scale;
+	while (true) {
+		pushButton();
+		if (feedHits.reduce((acc, val) => acc * val, 1)) break;
+	}
+
+	answers[1] = feedHits.reduce((acc, val) => {
+		const gcd = (a, b) => (b ? gcd(b, a % b) : a);
+		return (acc * val) / gcd(acc, val);
+	}, 1);
 
 	return answers;
 };
