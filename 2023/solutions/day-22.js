6a154d084a1320e1709db421892262ed9cf8e45e
Bret Hudson <bret@brethudson.com>
2024-06-11 16:03:13 -0700

    2023 Day 22 / Part 2

--- 2023/solutions/day-22.js
+++ 2023/solutions/day-22.js
@@ -7,18 +7,29 @@ export const solution = (input) => {
 			.map((coord) => coord.split(',').map((v) => +v));
 		const [[x1, y1, z1], [x2, y2, z2]] = coords;
 		const id = String.fromCharCode(65 + i);
-		return { id, x1, y1, z1, x2, y2, z2 };
+		return {
+			id,
+			x1,
+			y1,
+			z1,
+			x2,
+			y2,
+			z2,
+			supports: [],
+			supportedBy: [],
+		};
 	});
 
 	const overlap = (a, b, z = 0) => {
-		return [
-			a.x1 <= b.x2,
-			a.x2 >= b.x1,
-			a.y1 <= b.y2,
-			a.y2 >= b.y1,
-			a.z1 - z <= b.z2,
-			a.z2 - z >= b.z1,
-		].reduce((acc, val) => acc && val, true);
+		return (
+			// check Z first since it's the most likely to fail (early exit :))
+			a.z1 - z <= b.z2 &&
+			a.z2 - z >= b.z1 &&
+			a.x1 <= b.x2 &&
+			a.x2 >= b.x1 &&
+			a.y1 <= b.y2 &&
+			a.y2 >= b.y1
+		);
 	};
 
 	const canFall = (brick, brickIndex, ignore = -1) => {
@@ -33,33 +44,61 @@ export const solution = (input) => {
 		return !atBottom && free;
 	};
 
-	const fall = (brick) => {
-		--brick.z1;
-		--brick.z2;
-	};
-
+	// this is really slow, could speed up by partitioning the blocks by their Z value(s)
 	let fell = true;
 	while (fell) {
 		fell = false;
 		for (let i = 0; i < bricks.length; ++i) {
 			if (canFall(bricks[i], i)) {
-				fall(bricks[i]);
+				--bricks[i].z1;
+				--bricks[i].z2;
 				fell = true;
-				bricks[i];
 			}
 		}
 	}
 
-	let canDisintegrate = 0;
-	for (let skip = 0; skip < bricks.length; ++skip) {
-		let fall = false;
+	// find which bricks support which other bricks
+	for (let j = 0; j < bricks.length; ++j) {
 		for (let i = 0; i < bricks.length; ++i) {
-			fall ||= canFall(bricks[i], i, skip);
+			if (i === j) continue;
+			if (overlap(bricks[j], bricks[i], 1)) {
+				// bricks[i] is supporting j
+				bricks[i].supports.push(bricks[j]);
+				bricks[j].supportedBy.push(bricks[i]);
+			}
 		}
-		if (!fall) ++canDisintegrate;
 	}
 
-	answers[0] = canDisintegrate;
+	const unsafeBricks = bricks.filter((brick) => {
+		const { supports } = brick;
+		return supports.some((b) => b.supportedBy.length === 1);
+	});
+
+	const chainReactions = unsafeBricks.map((brick) => {
+		const queue = [];
+		queue.push(brick);
+		let count = 0;
+		const demolished = new Set(brick.id);
+		while (queue.length) {
+			const { supports } = queue.shift();
+			for (let i = 0; i < supports.length; ++i) {
+				const s = supports[i];
+				const hash = s.id;
+				const belowDestroyed = s.supportedBy.every((brick) =>
+					demolished.has(brick.id),
+				);
+				if (!belowDestroyed) continue;
+				if (demolished.has(hash)) continue;
+				demolished.add(hash);
+				++count;
+				queue.push(s);
+			}
+		}
+		return count;
+	});
+
+	answers[0] = bricks.length - unsafeBricks.length;
+	answers[1] = chainReactions.reduce((acc, val) => acc + val, 0);
 
 	return answers;
 };
