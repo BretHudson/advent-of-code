e96e8ae421f0328c439a594d2f072c57dc28cfd6
Bret Hudson <bret@brethudson.com>
2024-06-10 22:13:41 -0700

    2023 Day 21 / Part 2

--- 2023/solutions/day-21.js
+++ 2023/solutions/day-21.js
@@ -10,9 +10,6 @@ export const solution = (input) => {
 		.map((line, i) => [line.indexOf('S'), i])
 		.find(([x]) => x !== -1);
 
-	const EVEN = 0;
-	const ODD = 1;
-
 	grid[startPos[1]][startPos[0]] = '.';
 
 	const setGrid = (x, y, value) => {
@@ -21,24 +18,38 @@ export const solution = (input) => {
 		if (grid[y][x] === '.') grid[y][x] = value;
 	};
 
-	setGrid(...startPos, EVEN);
+	setGrid(...startPos, 0);
 
-	for (let s = 1; s <= 64; ++s) {
-		const last = s % 2 ? EVEN : ODD;
-		const next = s % 2 ? ODD : EVEN;
+	for (let s = 1; s <= 1310; ++s) {
 		for (let y = 0; y < height; ++y) {
 			for (let x = 0; x < width; ++x) {
-				if (grid[y][x] === last) {
-					setGrid(x - 1, y, next);
-					setGrid(x + 1, y, next);
-					setGrid(x, y - 1, next);
-					setGrid(x, y + 1, next);
+				if (grid[y][x] === s - 1) {
+					setGrid(x - 1, y, s);
+					setGrid(x + 1, y, s);
+					setGrid(x, y - 1, s);
+					setGrid(x, y + 1, s);
 				}
 			}
 		}
 	}
 
-	answers[0] = grid.flat().filter((v) => v === 0).length;
+	answers[0] = grid.flat().filter((v) => v % 2 === 0 && v <= 64).length;
+
+	const maxSteps = 26501365;
+
+	// Kudos to https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21 for the assist
+	const evenC = grid.flat().filter((v) => v % 2 === 0 && v > 65).length;
+	const oddC = grid.flat().filter((v) => v % 2 === 1 && v > 65).length;
+	const evenF = grid.flat().filter((v) => v % 2 === 0).length;
+	const oddF = grid.flat().filter((v) => v % 2 === 1).length;
+	const n = (maxSteps - (width >> 1)) / width;
+
+	answers[1] = [
+		(n + 1) * (n + 1) * oddF,
+		n * n * evenF,
+		-(n + 1) * oddC,
+		n * evenC,
+	].reduce((acc, val) => acc + val);
 
 	return answers;
 };
