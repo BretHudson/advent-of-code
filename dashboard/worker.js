bd1987d41a8aaf97514aca672ac77432f086b492
Bret Hudson <bret@brethudson.com>
2025-01-07 23:49:05 -0800

    Added timing data to dashboard

--- dashboard/worker.js
+++ dashboard/worker.js
@@ -6,20 +6,29 @@ addEventListener('message', async (e) => {
 	const process = config.process(year);
 
 	// console.log(`Running against input "${input}"`);
+	let start = performance.now();
 	try {
 		const fileNameUnique = `/${fileName}?${Date.now().toString(36)}`;
 		if (!process) {
 			const { solution } = await import(fileNameUnique);
 			const answers = solution(input);
-			postMessage({ success: true, answers });
+			postMessage({
+				success: true,
+				answers,
+				duration: performance.now() - start,
+			});
 			return;
 		}
 
 		process(input, fileNameUnique, (success, answers) => {
-			postMessage({ success, answers });
+			postMessage({
+				success,
+				answers,
+				duration: performance.now() - start,
+			});
 		});
 	} catch (e) {
 		console.error(e);
-		postMessage({ success: false });
+		postMessage({ success: false, duration: performance.now() - start });
 	}
 });
